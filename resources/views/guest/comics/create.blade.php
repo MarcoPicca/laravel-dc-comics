1c90fc72867edbb537b1591e65701a497149453d
MarcoPicca <il96marco@gmail.com>
2024-02-06 19:18:27 +0100

    laravel-dc-comics create-store

--- resources/views/guest/comics/create.blade.php
+++ resources/views/guest/comics/create.blade.php
@@ -1,48 +1,48 @@
 @extends('layouts.app')
 
 @section('main-content')
-<form action="{route(comics.store)}" method='POST'>
+<form action="{{ route('guest.comics.store') }}" method='POST'>
     @csrf    
 
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputEmail4">title</label>
-      <input type="text" class="form-control" id="inputEmail4" placeholder="title">
+      <label for="name">title</label>
+      <input type="text" name="title" class="form-control" id="title" placeholder="title">
     </div>
     <div class="form-group col-md-6">
-      <label for="inputPassword4">description</label>
-      <input type="text" class="form-control" id="inputPassword4" placeholder="description of your comic">
+      <label for="name">description</label>
+      <input type="text" name="description" class="form-control" id="description" placeholder="description of your comic">
     </div>
   </div>
   <div class="form-group">
-    <label for="inputAddress">thumb</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="image of your comic">
+    <label for="name">thumb</label>
+    <input type="text" name="thumb" class="form-control" id="thumb" placeholder="image of your comic">
   </div>
   <div class="form-group">
-    <label for="inputAddress2">price</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="price of your comic">
+    <label for="name">price</label>
+    <input type="text" name="price" class="form-control" id="price" placeholder="price of your comic">
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputCity">series</label>
-      <input type="text" class="form-control" id="inputCity">
+      <label for="name">series</label>
+      <input type="text" class="form-control" name="series" id="series">
     </div>
     
     <div class="form-group col-md-2">
-      <label for="inputZip">sale_date</label>
-      <input type="text" class="form-control" id="inputZip">
+      <label for="name">sale_date</label>
+      <input type="text" name="sale_date" class="form-control" id="sale_date">
     </div>
     <div class="form-group col-md-2">
-      <label for="inputZip">type</label>
-      <input type="text" class="form-control" id="inputZip">
+      <label for="name">type</label>
+      <input type="text" name="type" class="form-control" id="type">
     </div>
     <div class="form-group col-md-2">
-      <label for="inputZip">artists</label>
-      <input type="text" class="form-control" id="inputZip">
+      <label for="name">artists</label>
+      <input type="text" name="artists" class="form-control" id="artists">
     </div>
     <div class="form-group col-md-2">
-      <label for="inputZip">writers</label>
-      <input type="text" class="form-control" id="inputZip">
+      <label for="name">writers</label>
+      <input type="text" name="writers" class="form-control" id="writers">
     </div>
   </div>
   <button type="submit" class="btn btn-primary">Crea Nuovo Fumetto </button>
